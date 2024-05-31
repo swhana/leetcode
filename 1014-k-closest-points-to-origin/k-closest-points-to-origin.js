a31998b91111485b1c8379f97f790e01a759d791
Han Sang Won <109811519+swhana@users.noreply.github.com>
2024-05-31 18:31:44 +0900

    Time: 148 ms (97.55%) | Memory: 60.9 MB (98.04%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -4,24 +4,7 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    let dists = new Map();
+    points.sort((a, b) => (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
 
-    for(let [x, y] of points) {
-        const dist = x*x + y*y;
-        if(!dists.has(dist)) dists.set(dist, [[x, y]]);
-        else {
-            let arr = dists.get(dist);
-            arr.push([x, y]);
-            dists.set(dist, arr);
-        }
-    }
-
-    let newDists = [...dists].sort((a, b) => a[0] - b[0]); 
-
-    let distsArray = []
-    for(let i=0; i<newDists.length; i++) {
-        distsArray.push(...newDists[i][1]);
-    }
-
-    return distsArray.slice(0, k);
+    return points.slice(0, k);
 };
