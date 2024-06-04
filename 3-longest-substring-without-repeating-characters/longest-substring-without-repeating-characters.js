f956a424f0ad4cb015cdbd7cb89165aed3967ca4
Han Sang Won <109811519+swhana@users.noreply.github.com>
2024-06-04 10:05:39 +0900

    Time: 66 ms (97.65%) | Memory: 55.1 MB (40.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -6,7 +6,7 @@ var lengthOfLongestSubstring = function(s) {
     //currStr에 추가해가기
     //동시에 해시맵에도 문자 추가하기
     //최대 길이를 갱신하면서 가기
-    //중복되는 문자가 나오면 맨 앞과 맨 뒤가 같은 문자일테니 앞을 제거하고 뒤를 추가하면서 다시 이어나가기
+    //중복되는 문자가 나오면 해당 문자의 위치를 찾아 그 뒤부터 다시 이어나가기
     let currStr = ""
     let strMap = new Map();
     let maxLen = 0;
