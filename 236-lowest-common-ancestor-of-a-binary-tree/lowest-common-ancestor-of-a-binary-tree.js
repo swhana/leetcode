0afc0c79c40a99d3c6069c45f9a5c30c0df9dcc6
Han Sang Won <109811519+swhana@users.noreply.github.com>
2024-09-15 01:43:15 +0900

    Time: 72 ms (34.32%) | Memory: 59.1 MB (49.50%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,6 @@
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  var resL = lowestCommonAncestor(root.left, p, q);
+  var resR = lowestCommonAncestor(root.right, p, q);
+  return (resL && resR) ? root : (resL || resR);
+}
