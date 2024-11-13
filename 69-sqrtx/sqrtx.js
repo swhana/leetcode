07901d7e8b8b3cdb54a197cf2b6e33f21ee25074
Han Sang Won <109811519+swhana@users.noreply.github.com>
2024-11-13 22:21:10 +0900

    Time: 2 ms (49.63%) | Memory: 52.2 MB (87.01%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -2,24 +2,17 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
+var mySqrt = function (x) {
     let lp = 0;
     let rp = x;
 
-    let ret = 0;
-
-    while(lp <= rp) {
+    while (lp <= rp) {
         const mid = Math.floor((lp + rp) / 2);
         const pow = mid * mid;
-
-        console.log(lp, rp, mid);
-        if(pow === x) return mid;
-        if(pow < x) {
-            lp = mid + 1;
-            ret = Math.max(ret, mid);
-        }
-        if(pow > x) rp = mid - 1;
+        if (pow === x) return mid;
+        if (pow < x) lp = mid + 1;
+        if (pow > x) rp = mid - 1;
     }
 
-    return lp - 1; //lp를 return해야 소숫점일 때 내림한 값을 리턴함
+    return lp - 1; //lp - 1을 return해야 소숫점일 때 내림한 값을 리턴함
 };
